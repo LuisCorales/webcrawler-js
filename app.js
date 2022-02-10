128f79d2fd24c461b6c21314c354f690494d5734
Luis Corales <luisaugcorales@gmail.com>
2022-02-10 00:13:33 -0500

    New logic for unscraped scores

--- app.js
+++ app.js
@@ -9,10 +9,13 @@ const url = 'https://news.ycombinator.com/';
     await page.goto(url);
 
     const data = await page.evaluate(() => {
+        // If there is some news without scores, then proceed to do another process
+        let execAlt = document.querySelectorAll('.subtext .score').length < 30 ? true : false;
+
         const orderElements = document.querySelectorAll('.athing .title .rank');
         const titleElements = document.querySelectorAll('.athing .title a.titleLink');
-        const scoreElements = document.querySelectorAll('.subtext .score');
         const commentsElements = document.querySelectorAll('.subtext');
+        const scoreElements = document.querySelectorAll('.subtext .score');
         
         const orderRows = [];
         for (let element of orderElements)
@@ -26,20 +29,18 @@ const url = 'https://news.ycombinator.com/';
             titleRows.push(element.innerText);
         }
 
+        const commentRows = [];
+        for (let element of commentsElements)
+        {
+            let text = element.lastElementChild.innerText.match(/\d+/g);
+            commentRows.push(text != null ? parseInt(text[0]) : 0);
+        }   
+
         const scoreRows = [];
         for (let element of scoreElements)
         {
             scoreRows.push(parseInt(element.innerText.split(" ")[0]));
         }
-
-        const commentRows = [];
-        for (let element of commentsElements)
-        {
-            let text = element.lastElementChild.innerText.match(/\d+/g);
-            let comment = text != null ? parseInt(text[0]) : 0;
-
-            commentRows.push(comment);
-        }   
 
         const dataRow = [];
         for (let i = 0; i < 30; i++) {
@@ -52,7 +53,7 @@ const url = 'https://news.ycombinator.com/';
             dataRow.push(tmp);
         }
 
-        return [dataRow];
+        return [dataRow, execAlt];
     });
 
     console.log(data);
