e97f00e58556ffa5350602f49d1739e826c34c70
Luis Corales <luisaugcorales@gmail.com>
2022-02-10 16:09:26 -0500

    Scrape all data
    Be able to scrape all data without any errors and return it as a objects array

--- app.js
+++ app.js
@@ -2,20 +2,39 @@ const puppeteer = require('puppeteer');
 
 const url = 'https://news.ycombinator.com/';
 
-(async () => {
+const scrapeData = (async (url) => 
+{
     const browser = await puppeteer.launch({headless: true});
     const page = await browser.newPage();
 
     await page.goto(url);
 
-    const data = await page.evaluate(() => {
-        // If there is some news without scores, then proceed to do another process
-        let execAlt = document.querySelectorAll('.subtext .score').length < 30 ? true : false;
-
+    const data = await page.evaluate(() => 
+    {
+        // Get all needed data elements
         const orderElements = document.querySelectorAll('.athing .title .rank');
         const titleElements = document.querySelectorAll('.athing .title a.titleLink');
         const commentsElements = document.querySelectorAll('.subtext');
-        const scoreElements = document.querySelectorAll('.subtext .score');
+        const pointsElements = document.querySelectorAll('.subtext .score')
+
+        const pointsRows = [];
+
+        // If there is at least one news without scores, then proceed to do another process
+        if(pointsElements.length < 30)
+        {
+            for (let i = 0; i < commentsElements.length; i++) 
+            {
+                var numbersInInfo = commentsElements.item(i).innerText.match(/\d+/g);
+                pointsRows.push(numbersInInfo.length < 2 ? 0 : parseInt(numbersInInfo[0]));
+            }
+        }
+        else
+        {
+            for (let element of pointsElements)
+            {
+                pointsRows.push(parseInt(element.innerText.split(" ")[0]));
+            }
+        }
         
         const orderRows = [];
         for (let element of orderElements)
@@ -36,27 +55,81 @@ const url = 'https://news.ycombinator.com/';
             commentRows.push(text != null ? parseInt(text[0]) : 0);
         }   
 
-        const scoreRows = [];
-        for (let element of scoreElements)
-        {
-            scoreRows.push(parseInt(element.innerText.split(" ")[0]));
-        }
-
+        // Create an object of each news
         const dataRow = [];
         for (let i = 0; i < 30; i++) {
             const tmp = {};
             tmp.order = orderRows[i];
             tmp.title = titleRows[i];
             tmp.comments = commentRows[i];
-            tmp.score = scoreRows[i];
+            tmp.points = pointsRows[i];
 
             dataRow.push(tmp);
         }
 
-        return [dataRow, execAlt];
+        return dataRow;
     });
 
-    console.log(data);
-
     await browser.close();
+
+    // Return the objects array
+    return data;
+});
+
+const allNews = scrapeData(url);
+
+const filterByComments = (async (objectsArray) => 
+{
+    const filteredData = await objectsArray;
+    const toReturn = [];
+    let max = 0;
+    let min = 99999;
+
+    for (let i = 0; i < filteredData.length; i++) 
+    {
+        let title = filteredData[i].title;
+        //console.log(filteredData[i].title + " | " + filteredData[i].comments + " | " + filteredData[i].points);
+
+        // If the title has less than 6 words, then go to the next object
+        if (title.split(" ").length <= 5)
+        {
+            continue;
+        }
+
+        console.log(filteredData[i]);
+        if(filteredData[i].commets > max)
+        {
+            toReturn.push(filteredData[i]);
+        }
+        else
+        {
+            
+        }
+    }
+
+    return toReturn;
+});
+
+(async () => 
+{
+    console.log(await filterByComments(allNews));
 })();
+
+const filterByPoints = (async (objectsArray) => 
+{
+    const filteredData = [];
+
+    let max = 0;
+    let min = 99999;
+    for (let object in objectsArray) 
+    {
+        if (object.title.split(" ").length <= 5) 
+        {
+
+            console.log(object.title);
+            
+        }
+    }
+
+    //console.log(await objectsArray);
+});
